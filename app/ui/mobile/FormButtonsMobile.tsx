8b6570c4f95146c743d63ea7e6a4327b9e76624a
Justin Davies <therealjustindude@gmail.com>
2024-11-26 14:46:07 -0500

    mobile buttons move form along

--- app/ui/mobile/FormButtonsMobile.tsx
+++ app/ui/mobile/FormButtonsMobile.tsx
@@ -1,10 +1,25 @@
 'use client';
+import { formSteps } from '@/app/util/form-steps';
 import clsx from 'clsx';
-import { usePathname } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 
 export const FormButtonsMobile = () => {
 	const pathname = usePathname();
+	const router = useRouter();
+	const currStep = formSteps.find(step => step.pathname === pathname);
 
+	const handleBackClick = () => {
+		console.log('clicked back')
+		if (currStep?.prevStep) {
+			router.push(currStep?.prevStep)
+		}
+	}
+	const handleNextClick = () => {
+		console.log('clicked next')
+		if (currStep?.nextStep) {
+			router.push(currStep?.nextStep)
+		}
+	}
 	return (
 		<footer className={clsx("sticky bottom-0 w-full bg-white p-4 border-t md:hidden flex items-center justify-between",
 			{
@@ -14,10 +29,20 @@ export const FormButtonsMobile = () => {
 			<button 
 				className="btn bg-transparent text-cool-gray hover:bg-transparent hover:text-purplish-blue" 
 				style={{ visibility: pathname !== '/step-one' ? 'visible' : 'hidden' }}
+				onClick={handleBackClick}
 			>
 				Go Back
 			</button>
-			<button className="ml-auto">Next Step</button>
+			<button
+				className={clsx("btn ml-auto", 
+					{
+						'bg-purplish-blue hover:bg-purplish-blue opacity-90': currStep?.nextStep === null
+					}
+				)}
+				onClick={handleNextClick}
+			>
+				{currStep?.nextStep === null ? 'Confirm' : 'Next Step'}
+			</button>
 		</footer>
 	);
 }
