4abab70e8dcc98e917c6f80d8a24c7f5e65e47f4
Justin Davies <therealjustindude@gmail.com>
2024-11-26 21:25:09 -0500

    buttons using form context and toasts for error messages

--- app/ui/desktop/FormButtonsDesktop.tsx
+++ app/ui/desktop/FormButtonsDesktop.tsx
@@ -1,11 +1,15 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 'use client';
 import { usePathname, useRouter } from "next/navigation";
 import clsx from 'clsx';
 import { formSteps } from "@/app/util/form-steps";
+import { useFormContext } from "react-hook-form";
+import { toast } from "react-toastify";
 
 export const FormButtonsDesktop = () => {
 	const pathname = usePathname();
 	const router = useRouter();
+	const { handleSubmit, formState: { errors }, trigger } = useFormContext();
 	const currStep = formSteps.find(step => step.pathname === pathname);
 
 	const handleBackClick = () => {
@@ -14,12 +18,32 @@ export const FormButtonsDesktop = () => {
 			router.push(currStep?.prevStep)
 		}
 	}
-	const handleNextClick = () => {
-		console.log('clicked next')
-		if (currStep?.nextStep) {
-			router.push(currStep?.nextStep)
-		}
-	}
+
+	const onSubmit = (data: any) => {
+		console.log('Form submitted with data:', data);
+		// send this data somewhere
+  };
+	const handleConfirmClick = handleSubmit(onSubmit);
+
+	const handleNextClick = async () => {
+    const isValid = await trigger();
+
+    if (isValid) {
+      if (currStep?.nextStep) {
+        router.push(currStep?.nextStep);
+      }
+		} else {
+      const errorMessages = Object.values(errors)
+        .map((error: any) => error.message); 
+
+			if (errorMessages.length > 0) {
+				errorMessages.forEach(message => toast.error(`${message}`));
+			} else {
+				toast.error('There are validation errors.');
+			}
+    }
+  };
+
 	return (
 		<div
 			className={clsx("flex justify-between items-center hidden md:flex",
@@ -29,22 +53,30 @@ export const FormButtonsDesktop = () => {
 			)}
 		>
 			<button 
+				type="button"
 				className={"btn bg-transparent text-cool-gray hover:bg-transparent hover:text-purplish-blue"} 
 				style={{ visibility: pathname !== '/step-one' ? 'visible' : 'hidden' }}
 				onClick={handleBackClick}
 			>
 				Go Back
 			</button>
-			<button
-				className={clsx("btn ml-auto", 
-					{
-						'bg-purplish-blue hover:bg-purplish-blue opacity-90': currStep?.nextStep === null
-					}
-				)}
+			{currStep?.nextStep === null ? (
+				<button
+					type="button"
+					className="btn ml-auto bg-purplish-blue hover:bg-purplish-blue opacity-90"
+					onClick={handleConfirmClick}
+				>
+					Confirm
+				</button>
+			) : (
+				<button
+				type="button"
+				className="btn ml-auto"
 				onClick={handleNextClick}
 			>
-				{currStep?.nextStep === null ? 'Confirm' : 'Next Step'}
+				Next Step
 			</button>
+			)}
 		</div>
 	);
 }
--- app/ui/mobile/FormButtonsMobile.tsx
+++ app/ui/mobile/FormButtonsMobile.tsx
@@ -1,11 +1,15 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 'use client';
 import { formSteps } from '@/app/util/form-steps';
 import clsx from 'clsx';
 import { usePathname, useRouter } from 'next/navigation';
+import { useFormContext } from 'react-hook-form';
+import { toast } from 'react-toastify';
 
 export const FormButtonsMobile = () => {
 	const pathname = usePathname();
 	const router = useRouter();
+	const { handleSubmit, formState: { errors }, trigger} = useFormContext();
 	const currStep = formSteps.find(step => step.pathname === pathname);
 
 	const handleBackClick = () => {
@@ -14,12 +18,32 @@ export const FormButtonsMobile = () => {
 			router.push(currStep?.prevStep)
 		}
 	}
-	const handleNextClick = () => {
-		console.log('clicked next')
-		if (currStep?.nextStep) {
-			router.push(currStep?.nextStep)
-		}
-	}
+	
+	const onSubmit = (data: any) => {
+		console.log('Form submitted with data:', data);
+		// send this data somewhere
+  };
+	const handleConfirmClick = handleSubmit(onSubmit);
+
+	const handleNextClick = async () => {
+    const isValid = await trigger();
+
+    if (isValid) {
+      if (currStep?.nextStep) {
+        router.push(currStep?.nextStep);
+      }
+		} else {
+      const errorMessages = Object.values(errors)
+        .map((error: any) => error.message); 
+
+			if (errorMessages.length > 0) {
+				errorMessages.forEach(message => toast.error(`${message}`));
+			} else {
+				toast.error('There are validation errors.');
+			}
+    }
+	};
+	
 	return (
 		<footer className={clsx("sticky bottom-0 w-full bg-white p-4 border-t md:hidden flex items-center justify-between",
 			{
@@ -33,16 +57,23 @@ export const FormButtonsMobile = () => {
 			>
 				Go Back
 			</button>
-			<button
-				className={clsx("btn ml-auto", 
-					{
-						'bg-purplish-blue hover:bg-purplish-blue opacity-90': currStep?.nextStep === null
-					}
-				)}
+			{currStep?.nextStep === null ? (
+				<button
+					type="button"
+					className="btn ml-auto bg-purplish-blue hover:bg-purplish-blue opacity-90"
+					onClick={handleConfirmClick}
+				>
+					Confirm
+				</button>
+			) : (
+				<button
+				type="button"
+				className="btn ml-auto"
 				onClick={handleNextClick}
 			>
-				{currStep?.nextStep === null ? 'Confirm' : 'Next Step'}
+				Next Step
 			</button>
+			)}
 		</footer>
 	);
 }
